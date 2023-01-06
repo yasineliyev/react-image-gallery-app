16c48eb69ba5f42f83fd37531d036a27846d22e2
Yasin <105815296+yasineliyev@users.noreply.github.com>
2023-01-06 21:34:59 +0400

    first commit

---
+++ src/Images.js
@@ -0,0 +1,9 @@
+const Images = [
+  "https://mir-s3-cdn-cf.behance.net/projects/404/061d7785038549.Y3JvcCwzMTI5LDI0NDcsNjksMA.jpg",
+  "https://wallpaperaccess.com/full/1973841.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdAKgusEz1n3TjsA2MeDHgTtb284K1NWThAg&usqp=CAU",
+  "https://img.fcbayern.com/image/upload/t_cms-16x9-seo/v1665841756/cms/public/images/fcbayern-com/homepage/platzhalter/22-23-pm-fc-bayern.jpg",
+  "https://wallpaperaccess.com/full/777518.jpg",
+];
+
+export default Images;
